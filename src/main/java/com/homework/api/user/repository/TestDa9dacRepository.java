45b5004b715824450ce33bf48355b4e4b5e5bb71
da9dac <da9dac@gmail.com>
2024-06-04 16:11:14 +0900

    과제2: 수정, 다중 삭제, 새로고침 없이 테이블 갱신

--- src/main/java/com/homework/api/user/repository/TestDa9dacRepository.java
+++ src/main/java/com/homework/api/user/repository/TestDa9dacRepository.java
@@ -1,10 +1,21 @@
 package com.homework.api.user.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.homework.api.user.model.TestDa9dac;
 
 @Repository
 public interface TestDa9dacRepository extends JpaRepository<TestDa9dac, String> {
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE TestDa9dac t SET t.useYn = 'N', t.updaUser = :updaUser WHERE t.userId IN :userIds")
+	void updateUseYnAndUpdaUserByUserIdIn(@Param("userIds") List<String> userIds, String updaUser);
 }
